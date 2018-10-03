34a8e7c935b52de94759655dbd08f8959bc69c08
diegocalero0 <diegoacalero0@gmail.com>
2018-10-03 00:41:47 -0500

    deploy 1

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ var routerpage = require('./src/routes/routespage');
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, './src/views'));
 
+app.use(express.static(path.join(__dirname, 'src/public')));
+app.use('/static', express.static(path.join(__dirname, 'src/public')));
 
-console.log(path.join(__dirname, path.join(__dirname, '../src/reactcomponents')));
 app.use('/', routerpage);
 
-server.listen(process.argv[2], function(){
-	console.log("Corriendo en el servidor - puerto:" + process.argv[2]);
+server.listen(process.env.PORT, function(){
+	console.log("Corriendo en el servidor - puerto:" + process.env.PORT);
 });
